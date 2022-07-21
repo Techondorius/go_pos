23307de4dd84b2685853810247ae23bd2f73ebdc
Kyosuke Fujita <kurofuku01@gmail.com>
2022-07-21 10:46:00 +0900

    詳細記述

--- model/structs.go
+++ model/structs.go
@@ -3,22 +3,22 @@ package model
 // import "gorm.io/gorm"
 
 type ItemMaster struct {
-	ID int
-	Name string
-	BigCategory string
-	SmallCategory string
-	Grills []GrillPossible `gorm:"foreignKey:ItemID;references:ID"`
+	ID int `gorm:"primaryKey"`
+	Name string `gorm:"not null"`
+	BigCategory string `gorm:"not null"`
+	SmallCategory string `gorm:"not null"`
+	Grills []GrillPossible `gorm:"foreignKey:ItemID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
 
 type GrillPossible struct{
-	ID int
-	ItemID int
-	MasterID int
+	ID int `gorm:"primaryKey"`
+	ItemID int `gorm:"not null"`
+	MasterID int `gorm:"not null"`
 }
 
 type GrillMaster struct{
-	ID int
-	Type string
-	Name string
-	Grills []GrillPossible `gorm:"foreignKey:MasterID;references:ID"`
+	ID int `gorm:"primaryKey"`
+	Type string `gorm:"not null"`
+	Name string `gorm:"not null"`
+	Grills []GrillPossible `gorm:"foreignKey:MasterID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
